598ae8de570b7ea45597b03d47d3187f3b85e6c1
gabrielhsribeiro <ribeiro.gabrielhs@gmail.com>
2024-11-19 14:31:16 -0300

    V3 dos testes - inserindo phpunit

---
+++ tests/Service/AvaliadorTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Alura\Leilao\Tests\Service;
+
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use Alura\Leilao\Service\Avaliador;
+use PHPUnit\Framework\TestCase;
+
+//vendor/bin/phpunit --colors tests
+
+class AvaliadorTest extends TestCase
+{
+
+    public function testAvaliadorDeveEncontrarOMaiorValorDeLancesEmOrdemCrescente()
+    {
+        // Arrange - Given / Preparamos o cenário do teste
+        $leilao = new Leilao('Fiat 147 0km');
+
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('Joao');
+
+        $leilao->recebeLance(new Lance($joao, 2000));
+        $leilao->recebeLance(new Lance($maria, 2500));
+
+        $leiloeiro = new Avaliador();
+
+        // Act - When / Executamos o código a ser testado
+        $leiloeiro->avalia($leilao);
+
+        $maiorValor = $leiloeiro->getMaiorValor();
+
+        // Assert - Then / Verificamos se a saída é a esperada
+        $valorEsperado = 2500;
+
+        self::assertEquals(2500, $maiorValor);
+    }
+
+    public function testAvaliadorDeveEncontrarOMaiorValorDeLancesEmOrdemDecrescente()
+    {
+        // Arrange - Given / Preparamos o cenário do teste
+        $leilao = new Leilao('Fiat 147 0km');
+
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('Joao');
+
+        $leilao->recebeLance(new Lance($joao, 2000));
+        $leilao->recebeLance(new Lance($joao, 2000));
+
+        $leiloeiro = new Avaliador();
+
+        // Act - When / Executamos o código a ser testado
+        $leiloeiro->avalia($leilao);
+
+        $maiorValor = $leiloeiro->getMaiorValor();
+
+        // Assert - Then / Verificamos se a saída é a esperada
+        $valorEsperado = 2500;
+
+        self::assertEquals(2500, $maiorValor);
+    }
+}
